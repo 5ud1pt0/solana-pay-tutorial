014bed3827ae6155af87e169e8868811f4d9a1ca
5ud1pt0 <5ud1pt0@protonmail.com>
2022-05-01 16:50:42 +0530

    Initial deployment

---
+++ lib/addresses.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('2a7yaVbko3giSbyh5Cbvtfm1zmX9CdF1CzReMNSh5EoM') 
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('AA6xrCjM99FtHPBNiRwffsDfBuh7EJGWaMM38ScTUpCk')
+
